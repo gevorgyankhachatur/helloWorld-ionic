6d7192253a6918679ca3b36f74b7851bc297c3a7
Khachatur Gevorgyan <khachatur.gevorgyan.1993@gmail.com>
2021-11-05 20:57:36 +0400

    final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  name: string | undefined;
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+  onInput(event?: any) {
+    this.name = event.target.value;
+  }
+
+  showAlert() {
+    this.alertController
+      .create({
+        //header: '',
+        //subHeader: '',
+        message: `Hello ${this.name}`,
+        buttons: ['Thank you'],
+      })
+      .then((res) => {
+        res.present();
+      });
+  }
 }
