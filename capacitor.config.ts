184223d9151ff7926fd891954400aa8d76065426
Khachatur Gevorgyan <khachatur.gevorgyan.1993@gmail.com>
2021-11-06 00:59:16 +0400

    restyling 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,9 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'helloWorld',
+  appName: 'helloFromWorkMovr',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
